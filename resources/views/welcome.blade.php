ae44bf962a9d3a00d3c75f1fcae77773e33e2272
Yasin <6006586@mborijnland.nl>
2022-05-24 14:28:09 +0200

    Laravel crud

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="language" content="NL">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Yasin">
+    <meta name="author" content="Yasin">
+    <meta name="keywords" content="">
+    <title>C-R-U-D</title>
+    <link rel="stylesheet" type="text/css" href="./css/app.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body class="container mt-1">
+
+<header class=" row w-100 h-100 align-items-center ">
+    <article class="col-md-2  logo pt-3">
+        <a href="http://127.0.0.1:8000/products"><img src="https://mborijnland.nl/wp-content/uploads/2019/07/logo.svg" alt="logo"></a>
+    </article>
+    <nav class="nav col-md-10 d-flex justify-content-end navigatie-boven">
+        <a href="http://127.0.0.1:8000/products" class="nav-link">CRUD</a>
+
+
+    </nav>
+</header>
+<main>
+    <section class="row justify-content-center align-items-center ">
+        <article class="col-md-6 soon">
+            <img src="https://miro.medium.com/max/1200/1*gjA78w2_Q8lSNZAnTMScqA.png">
+        </article>
+        <article class="col-md-6 soon">
+            <img src="https://miro.medium.com/max/1200/1*gjA78w2_Q8lSNZAnTMScqA.png">
+        </article>
+        <article class="col-md-6 soon">
+            <img src="https://miro.medium.com/max/1200/1*gjA78w2_Q8lSNZAnTMScqA.png">
+        </article>
+        <article class="col-md-6 soon">
+            <img src="https://miro.medium.com/max/1200/1*gjA78w2_Q8lSNZAnTMScqA.png">
+        </article>
+    </section>
+</main>
+<footer>
+    <div class="mt-6 p-4 bg-dark text-white text-center">
+        <p>Created by Yasin Horani</p>
+    </div>
+</footer>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</html>
